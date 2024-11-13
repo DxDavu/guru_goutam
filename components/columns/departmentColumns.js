7c4267ee7aae9d28a9a65de2af5d1bddf05b3464
shahinsah2 <shahinsah@innogenx.in>
2024-11-13 10:34:56 +0530

    sl updated

--- components/columns/departmentColumns.js
+++ components/columns/departmentColumns.js
@@ -77,7 +77,8 @@ const DepartmentActionsCell = ({ row }) => {
 
 // Column definitions
 export const columns = [
-  { accessorKey: "slNo", header: "Sl.No." },
+  // { accessorKey: "slNo", header: "Sl.No." },
+  { id: "sl_no", header: "Sl. No", cell: ({ row }) => row.index + 1 },
   { accessorKey: "department_name", header: "Department" },
   { accessorKey: "description", header: "Description" },
   {
