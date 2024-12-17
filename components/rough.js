4f6a768053b775335f544bd53618813c0306a1ae
shahinsah2 <shahinsah@innogenx.in>
2024-12-17 15:29:38 +0530

    updated

--- components/rough.js
+++ components/rough.js
@@ -60,3 +60,9 @@ export const specificationData = {
       }
     ]
   }
+
+//   import DashboardSkeleton from '@/app/ui/skeletons';
+ 
+// export default function Loading() {
+//   return <DashboardSkeleton />;
+// }
